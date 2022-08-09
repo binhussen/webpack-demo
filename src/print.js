0e2b444c21e4660115fd5e283f3b143ac41b70d5
binhussen <mhdhsna@gmail.com>
2022-08-09 15:25:07 +0300

    Conf HtmlWEbPack

---
+++ src/print.js
@@ -0,0 +1,5 @@
+/** @format */
+
+export default function printMe() {
+	console.log("I get called from print.js!");
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 /** @format */
 
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: "./src/index.js",
+	entry: {
+		index: "./src/index.js",
+		print: "./src/print.js",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "Output Management",
+		}),
+	],
 	output: {
-		filename: "bundle.js",
+		filename: "[name].bundle.js",
 		path: path.resolve(__dirname, "dist"),
+		clean: true,
 	},
 };
