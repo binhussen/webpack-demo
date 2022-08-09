ea0c5f1192a3f5fe9b238109c910ffa7a7c3f0f8
binhussen <mhdhsna@gmail.com>
2022-08-09 14:58:56 +0300

    Loading Data

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import _ from "lodash";
 import "./style.css";
 import Icon from "./icon.png";
+import Data from "./data.xml";
+import Notes from "./data.csv";
 
 function component() {
 	const element = document.createElement("div");
@@ -15,6 +17,9 @@ function component() {
 	myIcon.src = Icon;
 
 	element.appendChild(myIcon);
+
+	console.log(Data);
+	console.log(Notes);
 
 	return element;
 }
