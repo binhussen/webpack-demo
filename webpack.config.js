e81c0238f1e6f1b1154c096cfc56b59f74015f0a
binhussen <mhdhsna@gmail.com>
2022-08-09 14:47:48 +0300

    Loading css

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+/** @format */
+
+const path = require("path");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
+};
